8b805edae42b01f955f064912703c9b433caebbb
Jerry V <jvr572@gmail.com>
2020-01-26 16:18:25 -0500

    Js interview Q's from leetcode

---
+++ leetCode/FizzBuzz.js
@@ -0,0 +1,24 @@
+/*
+classic fizzbuzz
+
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number 
+and for the multiples of five output “Buzz”. 
+For numbers which are multiples of both three and five output “FizzBuzz”.
+*/
+var fizzBuzz = function(n) {
+  numsArray = []
+  for(let i = 1; i <= n; i++ ){
+      if(i % 3 === 0 && !(i % 5 === 0)){
+          numsArray.push("Fizz") 
+      }else if(i % 5 === 0 && !(i % 3 === 0)){
+          numsArray.push("Buzz") 
+      }else if(i % 3 === 0 && i % 5 === 0){
+          numsArray.push("FizzBuzz") 
+      }else{
+          numsArray.push(i.toString())
+      }
+  }
+  return numsArray
+};
